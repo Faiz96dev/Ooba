42554f8f8ce1ca5ef258eeab1f168fd63fce4cc2
Faiz <faiz95dev@gmail.com>
2019-10-04 16:51:03 +0600

    repair for prod

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-$(document).ready(function(){
-  $(window).scroll(function () {
-         if ($(this).scrollTop() > 50) {
-             $('#back-to-top').fadeIn();
-         } else {
-             $('#back-to-top').fadeOut();
-         }
-     });
-     // scroll body to 0px on click
-     $('#back-to-top').click(function () {
-         $('#back-to-top').tooltip('hide');
-         $('body,html').animate({
-             scrollTop: 0
-         }, 800);
-         return false;
-     });
-     
-     $('#back-to-top').tooltip('show');
 
+$(document).ready(function() { 
+  var button = $('#button-up');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+});	 
+button.on('click', function(){
+$('body, html').animate({
+scrollTop: 0
+}, 800);
+return false;
+});		 
 });
+
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); 
+}
