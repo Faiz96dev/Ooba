26939fbe4891f27baf69e39c3dc03df47f4f08eb
Faiz <faiz95dev@gmail.com>
2019-10-05 10:13:17 +0600

    ready slider

--- js/script.js
+++ js/script.js
@@ -1,36 +1,22 @@
 
-$(document).ready(function() { 
-  var button = $('#button-up');	
-  $(window).scroll (function () {
-    if ($(this).scrollTop () > 300) {
+
+
+$(document).ready(function() {
+  var button = $("#button-up");
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
       button.fadeIn();
     } else {
       button.fadeOut();
     }
-});	 
-button.on('click', function(){
-$('body, html').animate({
-scrollTop: 0
-}, 800);
-return false;
-});		 
+  });
+  button.on("click", function() {
+    $("body, html").animate(
+      {
+        scrollTop: 0
+      },
+      800
+    );
+    return false;
+  });
 });
-
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); 
-}
