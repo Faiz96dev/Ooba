9a88d9e8c250de79f873164351ed24bf25257fa3
Faiz <faiz95dev@gmail.com>
2019-10-07 15:49:31 +0600

    add some

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function() {
   var button = $("#button-up");
   $(window).scroll(function() {
@@ -21,18 +18,46 @@ $(document).ready(function() {
   });
 });
 
-
 var http = new XMLHttpRequest();
-var url = 'get_data.php';
-var params = 'orem=ipsum&name=binny';
-http.open('POST', url, true);
+var url = "get_data.php";
+var params = "orem=ipsum&name=binny";
+http.open("POST", url, true);
 
 //Send the proper header information along with the request
-http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-}
+http.onreadystatechange = function() {
+  //Call a function when the state changes.
+  if (http.readyState == 4 && http.status == 200) {
+    alert(http.responseText);
+  }
+};
 http.send(params);
+
+$(document).ready(function() {
+  $("#mySidebar").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1200);
+  });
+});
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "210px";
+  document.getElementById("main").style.marginLeft = "210px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+$(document).ready(function() {
+  $(".autoplay").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+  });
+});
